ffd70200c57dd16b68ad1ce3b4ffd8548ca2ca7d
Burak Coskun <burakcosk@gmail.com>
2017-08-03 17:35:53 +0300

    implement sign and zipalign apk file

--- src/main/java/com/burakcoskun/litebuild/cli/CommandPackage.java
+++ src/main/java/com/burakcoskun/litebuild/cli/CommandPackage.java
@@ -1,21 +1,30 @@
 package com.burakcoskun.litebuild.cli;
 
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.burakcoskun.litebuild.commandbuilders.AndroidSDKCommandBuilder;
+import com.burakcoskun.litebuild.commandbuilders.JavaCommandBuilder;
+
+import java.io.File;
 
 /**
  * Created by burakcoskun on 8/2/17.
  */
-@Parameters(separators = "=", commandDescription = "Compiles source files, but first creates R.java then compiles source files then creates dex file.")
+@Parameters(separators = "=", commandDescription = "Creates package signs it with debug key and zipaligns the created package.")
 public class CommandPackage extends Command {
 
     AndroidSDKCommandBuilder androidSDKCommandBuilder;
+    JavaCommandBuilder javaCommandBuilder;
 
     public CommandPackage() {
         super();
         command = "package";
         androidSDKCommandBuilder = new AndroidSDKCommandBuilder();
+        javaCommandBuilder = new JavaCommandBuilder();
     }
+
+    @Parameter(names = {"-v","--validity"}, description = "Validity of debug key in days.")
+    private String validity;
 
     @Override
     public int runWithoutHelp() {
@@ -23,9 +32,12 @@ public class CommandPackage extends Command {
         runBeforeAfterCommands(true);
 
         processRunner.run(androidSDKCommandBuilder.packageCommand());
+        processRunner.run(javaCommandBuilder.signCommand());
+        processRunner.run(javaCommandBuilder.zipAlignApk());
 
         runBeforeAfterCommands(false);
 
         return 0;
     }
+
 }
--- src/main/java/com/burakcoskun/litebuild/commandbuilders/JavaCommandBuilder.java
+++ src/main/java/com/burakcoskun/litebuild/commandbuilders/JavaCommandBuilder.java
@@ -1,5 +1,6 @@
 package com.burakcoskun.litebuild.commandbuilders;
 
+import com.burakcoskun.litebuild.utils.BuildToolsLocator;
 import com.burakcoskun.litebuild.utils.ClassPathLocator;
 
 import java.util.List;
@@ -51,4 +52,32 @@ public class JavaCommandBuilder extends CommandBuilder {
         for (int i = 0; i < sourceFiles.size(); ++i)
             builder.append(" " + sourceFiles.get(i));
     }
+
+    public String signCommand() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(javaHome+"/bin/jarsigner");
+        builder.append(" -verbose");
+        builder.append(" -keystore "+System.getProperty("user.home")+"/.android/debug.keystore");
+        builder.append(" -storepass android");
+        builder.append(" -keypass android");
+        builder.append(" -signedjar bin/AndroidTest.signed.apk");
+        builder.append(" bin/AndroidTest.unsigned.apk");
+        builder.append(" androiddebugkey");
+        return builder.toString();
+    }
+
+    public String zipAlignApk(){
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(androidHome+"/build-tools/"+new BuildToolsLocator().getLatestAvailableBuildToolsVersion(androidHome));
+        builder.append("/zipalign");
+        builder.append(" -v");
+        builder.append(" -f");
+        builder.append(" 4");
+        builder.append(" bin/AndroidTest.signed.apk");
+        builder.append(" bin/AndroidTest.apk");
+
+        return builder.toString();
+    }
+
 }
